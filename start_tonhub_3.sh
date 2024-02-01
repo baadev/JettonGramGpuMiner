d2f4ee97e83c924215f81aa5d9dbd431a38385e7
Alexander <baadev15@gmail.com>
2024-02-02 00:11:46 +0200

    Update timeout values in start_tonhub scripts

--- start_tonhub_3.sh
+++ start_tonhub_3.sh
@@ -3,6 +3,6 @@ npm install
 
 
 while true; do
-  node send_universal.js --api tonhub --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout 2
+  node send_universal.js --api tonhub --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout 3
   sleep 1;
 done;
