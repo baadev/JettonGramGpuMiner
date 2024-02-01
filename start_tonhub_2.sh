eecb99a321eccbbb5fd2ca6b139182b48add7762
Alexander <baadev15@gmail.com>
2024-02-01 16:17:40 +0200

    increase timeout

--- start_tonhub_2.sh
+++ start_tonhub_2.sh
@@ -3,6 +3,6 @@ npm install
 
 
 while true; do
-  node send_universal.js --api tonhub --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout 1
+  node send_universal.js --api tonhub --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout 2
   sleep 1;
 done;
