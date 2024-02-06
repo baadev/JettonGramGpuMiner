6a6ba5cb204206a6b42591a9e4e52c4d07d8197a
Alexander <baadev15@gmail.com>
2024-02-06 11:25:52 +0200

    added new start scripts

---
+++ start_tonhub-tonapi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+npm install
+
+while true; do
+  pkill -f pow-miner-cuda;
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 0 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 1 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 4 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 5 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 6 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 7 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 8 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 9 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 10 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 11 --timeout 3 --api tonhub;
+  sleep 1;
+  # This part will be reached only if the previous part fails
+  pkill -f pow-miner-cuda;
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 0 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 1 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 4 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 5 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 6 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 7 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 8 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 9 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 10 --timeout 3 --api tonapi &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 11 --timeout 3 --api tonapi;
+  sleep 1;
+done;
--- start_tonhub.sh
+++ start_tonhub.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 npm install
 
-
 while true; do
-  node send_universal.js --api tonhub --bin ./pow-miner-cuda
+  pkill -f pow-miner-cuda;
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 0 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 1 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 4 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 5 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 6 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 7 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 8 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 9 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 10 --timeout 3 --api tonhub &
+  node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 11 --timeout 3 --api tonhub;
   sleep 1;
 done;
