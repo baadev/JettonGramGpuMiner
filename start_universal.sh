c486d7c0b583aad23229f865461b7b5a5444a08b
Alexander <baadev15@gmail.com>
2024-02-06 17:56:35 +0200

    added new start conf

--- start_universal.sh
+++ start_universal.sh
@@ -5,29 +5,29 @@ git pull;
 # time to wait until next attempt
 TIMEOUT=4;
 # tonhub | tonapi
-API=tonapi; 
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 0 --timeout $TIMEOUT --api $API &
+API=lite; 
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 0 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 1 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 1 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 2 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 3 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 4 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 4 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 5 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 5 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 6 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 6 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 7 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 7 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 8 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 8 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 9 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 9 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 10 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 10 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 11 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 11 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
 sleep 0.25 &&
-node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 12 --timeout $TIMEOUT --api $API &
+node send_universal.js --bin ./pow-miner-cuda --givers 1000 --gpu 12 --timeout $TIMEOUT --api $API -c "https://github.com/yusupovanton/mineconfig/raw/master/mineconfig.json" &
